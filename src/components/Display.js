050f223d0cfcec051a5298caa1aa763bbd04bf9d
bcast201 <bcast201@gmail.com>
2020-10-20 20:59:06 -0500

    first commit

---
+++ src/components/Display.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
+import large2 from '../img/large2.jpg';
+
+
+function Display() {
+    return (
+            <Container className='displayContainer'>
+                <Row>
+                    <Col md={6}>
+                        <img className='img-responsive displayImage img-fluid' alt='sandwich' src={large2}/>
+                    </Col>
+                    <Col md={6} className='displayFlex'>
+                    <div className='taglineContainer'>
+                        <p className='tagline'>Quality Meat</p>
+                        <p className='tagline'> Quality Cheeses</p>
+                        <p className='tagline'> NO BALONEY!!</p>
+                        <p className='tagline'> Sliced Fresh Every time!!</p>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+    )
+}
+
+export default Display;
