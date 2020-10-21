27a0e9f64c55c700cb8050478aaee72f6d19e6a3
bcast201 <bcast201@gmail.com>
2020-10-20 21:05:56 -0500

    Updates

---
+++ precache-manifest.df854a4202b567198c4998e889546203.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26790deaebf986efde968245095b3362",
+    "url": "/Dannys-Downtown-Deli/index.html"
+  },
+  {
+    "revision": "69fc87e8e841afcaf55d",
+    "url": "/Dannys-Downtown-Deli/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "10dbd24054b172f41e6a",
+    "url": "/Dannys-Downtown-Deli/static/css/main.70df4201.chunk.css"
+  },
+  {
+    "revision": "69fc87e8e841afcaf55d",
+    "url": "/Dannys-Downtown-Deli/static/js/2.e482d428.chunk.js"
+  },
+  {
+    "revision": "24a85d013fe3920224b2a90395887eff",
+    "url": "/Dannys-Downtown-Deli/static/js/2.e482d428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10dbd24054b172f41e6a",
+    "url": "/Dannys-Downtown-Deli/static/js/main.80eef2b4.chunk.js"
+  },
+  {
+    "revision": "a73e5bd3d20d60a943b0",
+    "url": "/Dannys-Downtown-Deli/static/js/runtime-main.314ca4ee.js"
+  },
+  {
+    "revision": "93409a4dcef3afe6818826a6a3d3f442",
+    "url": "/Dannys-Downtown-Deli/static/media/bigbellydeli.93409a4d.jpeg"
+  },
+  {
+    "revision": "b33ea2f4e54bf1a6d099a3cfd0eacdc9",
+    "url": "/Dannys-Downtown-Deli/static/media/large2.b33ea2f4.jpg"
+  },
+  {
+    "revision": "422e890b864ccff66872e0a36d6140ed",
+    "url": "/Dannys-Downtown-Deli/static/media/window art.422e890b.jpg"
+  }
+]);
